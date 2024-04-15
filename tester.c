fccde68bee7c315111489c5ae9b4c4b67b462f5f
Brendan Gasparin <brendan.gasparin@gmail.com>
2024-04-15 11:07:04 +1000

    Add skeletal tester program

---
+++ tester.c
@@ -0,0 +1,14 @@
+/*
+    tester.c
+    Author: Brendan Gasparin <brendan.gasparin@gmail.com>
+    Date: 15 April 2024
+    Testing program for getinput library.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Executing tester.\n");
+    printf("Exiting tester.\n");
+}
