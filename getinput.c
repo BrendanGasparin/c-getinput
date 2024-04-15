d53b63f536f8a8a8af440a581121267f073e263c
Brendan Gasparin <brendan.gasparin@gmail.com>
2024-04-16 02:58:47 +1000

    Implement skeletal getinput files

---
+++ getinput.c
@@ -0,0 +1,6 @@
+#include "getinput.h"
+
+string get_string(string prompt)
+{
+    return "\0";
+}
--- getinput.h
+++ getinput.h
@@ -3,4 +3,6 @@
 
 typedef char *string;
 
+string get_string(string prompt);
+
 #endif /* GETINPUT_H */
